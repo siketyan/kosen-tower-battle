cf633a28f08eb11971818b20f03db1c85f03af9f
Naoki Ikeguchi <root@siketyan.dev>
2020-10-23 23:48:51 +0900

    ✨ Matter.js demo

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { Bodies, Engine, Render, Runner, World } from 'matter-js';
+
+const engine = Engine.create();
+const renderer = Render.create({
+  element: document.getElementById('app'),
+  engine,
+  options: {
+    width: 800,
+    height: 600,
+    wireframes: false,
+    background: 'white',
+  },
+});
+
+Render.run(renderer);
+
+const runner = Runner.create();
+Runner.run(runner, engine);
+
+const ground = Bodies.rectangle(
+  renderer.options.width / 2,
+  renderer.options.height + 10,
+  renderer.options.width + 10,
+  60,
+  {
+    isStatic: true,
+  },
+);
+
+const box = Bodies.rectangle(400, 200, 80, 80);
+
+World.add(engine.world, [ground, box]);
