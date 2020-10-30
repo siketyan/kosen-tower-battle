5291731904b287f41a54423209492cdcc97533ab
Naoki Ikeguchi <root@siketyan.dev>
2020-10-30 23:27:38 +0900

    ✨ Add share link

--- src/components/molecules/end-screen/index.tsx
+++ src/components/molecules/end-screen/index.tsx
@@ -3,6 +3,15 @@ import * as QRCode from 'qrcode';
 
 import { Button, Overlay, Panel } from '../../atoms';
 
+const TITLE = '高専タワーバトル';
+const TEXT = '高専に関するものを積み上げよう！いったいどこまで高く積むことができるのか！？';
+const URL = 'https://ktb.pv6.jp/';
+
+const getSharezUrl = () => {
+  const e = encodeURIComponent;
+  return `https://s.pv6.jp/?text=${e(`${TITLE}: ${TEXT}`)}&url=${e(URL)}`;
+};
+
 export class EndScreen extends React.Component {
   private canvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
 
@@ -24,13 +33,28 @@ export class EndScreen extends React.Component {
             <hr />
             <section>
               <h4>シェア</h4>
-              <p>下の QR コードをスキャンするとソーシャルメディアに共有できます．</p>
-              <p>
-                <canvas
-                  id="qrcode"
-                  ref={ this.canvasRef }
-                />
-              </p>
+              {
+                location.href.includes('/ktb.pv6.jp/')
+                  ? (
+                    <>
+                      <p>下の QR コードをスキャンするとソーシャルメディアに共有できます．</p>
+                      <p>
+                        <canvas
+                          id="qrcode"
+                          ref={this.canvasRef}
+                        />
+                      </p>
+                    </>
+                  )
+                  : (
+                    <p>
+                      <Button
+                        text="共有"
+                        onClick={ () => location.href = getSharezUrl() }
+                      />
+                    </p>
+                  )
+              }
             </section>
           </>
         </Panel>
@@ -39,10 +63,8 @@ export class EndScreen extends React.Component {
   }
 
   async componentDidMount(): Promise<void> {
-    const e = encodeURIComponent;
-    await QRCode.toCanvas(
-      this.canvasRef.current,
-      `https://s.pv6.jp/?text=${e('高専タワーバトルに挑戦したよ！')}&url=${e(location.href)}`,
-    );
+    await QRCode.toCanvas(this.canvasRef.current, getSharezUrl());
   }
+
+  private static
 }
